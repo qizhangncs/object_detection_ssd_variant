ec0e5fffc63f55e3d25fb53068a515d72eec5efd
Qi Zhang <qi.zhang.ncs@gmail.com>
2019-04-01 16:46:36 -0400

    four classes

--- vision/datasets/voc_dataset.py
+++ vision/datasets/voc_dataset.py
@@ -22,13 +22,19 @@ class VOCDataset:
         self.ids = VOCDataset._read_image_ids(image_sets_file)
         self.keep_difficult = keep_difficult
 
+        # self.class_names = ('BACKGROUND',
+        #     'aeroplane', 'bicycle', 'bird', 'boat',
+        #     'bottle', 'bus', 'car', 'cat', 'chair',
+        #     'cow', 'diningtable', 'dog', 'horse',
+        #     'motorbike', 'person', 'pottedplant',
+        #     'sheep', 'sofa', 'train', 'tvmonitor'
+        # )
+
+        ## now just focuses on training the four classes: BACKGROUND, bicycle, car,person
         self.class_names = ('BACKGROUND',
-            'aeroplane', 'bicycle', 'bird', 'boat',
-            'bottle', 'bus', 'car', 'cat', 'chair',
-            'cow', 'diningtable', 'dog', 'horse',
-            'motorbike', 'person', 'pottedplant',
-            'sheep', 'sofa', 'train', 'tvmonitor'
+            'bicycle', 'car', 'person'
         )
+
         self.class_dict = {class_name: i for i, class_name in enumerate(self.class_names)}
 
     def __getitem__(self, index):
@@ -67,23 +73,24 @@ class VOCDataset:
         return ids
 
     def _get_annotation(self, image_id):
-        annotation_file = self.root / f"Annotations/{image_id}.xml"
+        annotation_file = self.root / f"Annotations/{image_id}.xml"#a single image
         objects = ET.parse(annotation_file).findall("object")
         boxes = []
         labels = []
         is_difficult = []
         for object in objects:
             class_name = object.find('name').text.lower().strip()
-            bbox = object.find('bndbox')
-            # VOC dataset format follows Matlab, in which indexes start from 0
-            x1 = float(bbox.find('xmin').text) - 1
-            y1 = float(bbox.find('ymin').text) - 1
-            x2 = float(bbox.find('xmax').text) - 1
-            y2 = float(bbox.find('ymax').text) - 1
-            boxes.append([x1, y1, x2, y2])
-            labels.append(self.class_dict[class_name])
-            is_difficult_str = object.find('difficult').text
-            is_difficult.append(int(is_difficult_str) if is_difficult_str else 0)
+            if class_name == 'bicycle' or class_name == 'car' or class_name == 'person':
+                bbox = object.find('bndbox')
+                # VOC dataset format follows Matlab, in which indexes start from 0
+                x1 = float(bbox.find('xmin').text) - 1
+                y1 = float(bbox.find('ymin').text) - 1
+                x2 = float(bbox.find('xmax').text) - 1
+                y2 = float(bbox.find('ymax').text) - 1
+                boxes.append([x1, y1, x2, y2])
+                labels.append(self.class_dict[class_name])
+                is_difficult_str = object.find('difficult').text
+                is_difficult.append(int(is_difficult_str) if is_difficult_str else 0)
 
         return (np.array(boxes, dtype=np.float32),
                 np.array(labels, dtype=np.int64),
